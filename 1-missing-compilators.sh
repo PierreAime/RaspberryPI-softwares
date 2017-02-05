e2692998511a4a566642fcebfe5a47412e053a10
Pierre-Aime <pierreaime.imbert@outlook.fr>
2017-02-05 13:33:05 +0000

    Create 1-missing-compilators.sh
    Installation des compilateurs et includes manquants frequemment utilises

---
+++ 1-missing-compilators.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+
+echo "Setup missing compilators"
+
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get install valac
+sudo apt-get install python-dev
+sudo apt-get install intltool
