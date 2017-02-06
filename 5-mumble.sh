6c7af6ed7830b063ca53f1d32e8864a8ebf2ea9c
Pierre-Aime <pierreaime.imbert@outlook.fr>
2017-02-06 17:33:43 +0000

    Logiciel Voice IP pour faibles debits (script v3)

---
+++ 5-mumble.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#V3 PIERRE-AIME
+
+sudo apt-get update
+
+echo "-------------------------------"
+echo "Setup Mumble"
+echo "-------------------------------"
+
+sudo apt-get install -y mumble
